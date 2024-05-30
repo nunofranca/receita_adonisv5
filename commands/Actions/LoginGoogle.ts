326ac6ee42fc5a68660b8832f6eb69c6e111f82b
Nuno França <contato@nunotech.com.br>
2024-05-30 04:34:31 -0300

    ..

--- commands/Actions/LoginGoogle.ts
+++ commands/Actions/LoginGoogle.ts
@@ -1,5 +1,7 @@
 const LoginGoogle = async (page, email) => {
-  await page.goto('https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com.br%2F&ec=GAZAmgQ&hl=pt-BR&ifkv=AaSxoQzmgJ1cLzhz_RiZ3Q_19cFM6u5VCAKcx4o-dsaHREFzhBKIGnayemNomS5mNUMjEekDd4kK&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S1275832598%3A1716502765207858&ddm=0', {timeout: 60000});
+  await page.goto('https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com.br%2F&ec=GAZAmgQ&hl=pt-BR&ifkv=AaSxoQzmgJ1cLzhz_RiZ3Q_19cFM6u5VCAKcx4o-dsaHREFzhBKIGnayemNomS5mNUMjEekDd4kK&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S1275832598%3A1716502765207858&ddm=0', {
+    waitUntil: 'networkidle0' // Espera até que não haja mais de 0 conexões de rede por pelo menos 500ms
+  });
 
   // @ts-ignore
   await page.waitForSelector('#identifierId', {visible: true});
