ce1334e51e32c24e296fe00b6291f6ed91a351a1
Nuno França <contato@nunotech.com.br>
2024-06-01 01:12:17 -0300

    ..

--- UserAgentBetano.ts
+++ UserAgentBetano.ts
@@ -1,4 +1,4 @@
-const UserArgentBetano = ()=>[
+const UserArgentBetano = () => [
   // Google Chrome
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36", // Windows
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36", // Windows
@@ -48,4 +48,5 @@ const UserArgentBetano = ()=>[
   "Mozilla/5.0 (Linux; Android 11; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36", // Google Pixel 4 XL
   "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.451
 ];
-module .exports = ViewPort
+module.exports = UserArgentBetano
+
