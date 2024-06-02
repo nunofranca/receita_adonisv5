ebdc997d65731f7cf463cd0b1f789f9780b623da
Nuno França <contato@nunotech.com.br>
2024-06-02 09:40:07 -0300

    ..

--- commands/CreateBetanoBetCommand.ts
+++ commands/CreateBetanoBetCommand.ts
@@ -137,7 +137,7 @@ export default class TestPixSimple extends BaseCommand {
       //executablePath: '/usr/bin/chromium-browser',
       slowMo: 10,
       defaultViewport: null,
-      headless: true,
+      headless: false,
       ignoreDefaultArgs: ["--disable-extensions"],
 
       args: [
