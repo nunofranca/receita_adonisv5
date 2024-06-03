385f299be30c86f4ffc6b3698d32acdf15dd02d4
Nuno França <contato@nunotech.com.br>
2024-06-03 12:31:50 -0300

    ..

--- commands/CreateBetanoBetCommand.ts
+++ commands/CreateBetanoBetCommand.ts
@@ -610,26 +610,36 @@ export default class TestPixSimple extends BaseCommand {
 
       const notBot = await page.evaluate((text: string) => {
         // @ts-ignore
-        return document.body.textContent.includes(text);
+        const not =  document.body.textContent.includes(text);
+        if(not) console.log('Confirme que você não é um robô')
+        return not
       }, 'Confirme que você não é um robô');
 
       const captcha = await page.evaluate((text: string) => {
         // @ts-ignore
-        return document.body.textContent.includes(text);
+         document.body.textContent.includes(text);
+        const not =  document.body.textContent.includes(text);
+        if(not) console.log('CAPTCHA Security check')
+        return not
       }, 'CAPTCHA Security check');
       const emailExist = await page.evaluate((text: string) => {
-        // @ts-ignore
-        return document.body.textContent.includes(text);
+
+        const not =  document.body.textContent.includes(text);
+        if(not) console.log('Parece que já tem uma conta Betano. Por favor, digite sua senha para continuar')
+        return not
       }, 'Parece que já tem uma conta Betano. Por favor, digite sua senha para continuar.');
 
       const signin = await page.evaluate((text: string) => {
         // @ts-ignore
-        return document.body.textContent.includes(text);
+        const not =  document.body.textContent.includes(text);
+        if(not) console.log('Sign ink')
+        return not
       }, 'Sign in');
 
       const incom = await page.evaluate((text: string) => {
         // @ts-ignore
-        return document.body.textContent.includes(text);
+        const not =  document.body.textContent.includes(text);
+        if(not) console.log('Detectamos uma atividade')
       }, 'Detectamos uma atividade');
 
 
