81f72f985e1c26331cacd668f1ec8d87a1f42617
Nuno França <contato@nunotech.com.br>
2024-06-10 20:12:27 -0300

    ..

--- commands/CreateBetanoBetTesteCommand.ts
+++ commands/CreateBetanoBetTesteCommand.ts
@@ -98,29 +98,17 @@ export default class TestPixSimple extends BaseCommand {
       // env: {
       //   DISPLAY: ":0"
       // },
-      ignoreHTTPSErrors: true,
+
       //userDataDir: profileDir,
-      //executablePath: '/usr/bin/microsoft-edge',
+      executablePath: '/usr/bin/microsoft-edge',
       //executablePath: '/usr/bin/chrome-browser',
       // executablePath: '/usr/bin/chromium-browser',
-      slowMo: 10,
-      defaultViewport: null,
-      headless: false,
 
-      ignoreDefaultArgs: ["--disable-extensions"],
       args: [
-
         '--proxy-server=http://'+proxy.data.proxy,
         // '--start-maximized',
         '--start-minimized',
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu',
-        '--window-size=1920x1080',
-        '--disable-features=IsolateOrigins,site-per-process',
-
+        '--no-sandbox'
       ],
     });
 
